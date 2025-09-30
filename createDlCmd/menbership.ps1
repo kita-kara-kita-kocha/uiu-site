759720ff18030f73506a7ee76d7b9b52bb481c7e
kita-kara-kita-kocha <si.ro.ff2311@gmail.com>
2025-09-30 14:29:17 +0900

    コマンド変更検知するほうがいいやん

---
+++ createDlCmd/menbership.ps1
@@ -0,0 +1,40 @@
+# command to download youtube menbership video
+$video_url="https://www.youtube.com/watch?v=qbcMv-5KqEM"
+yt-dlp -o "【メン限】[%(upload_date)s]%(title)s.%(ext)s" $video_url -f bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a] -S vcodec:hevc_nvenc --cookies .\tmp\www.youtube.com_cookies.txt --download-archive acv
+
+# command to download youtube menbership video
+$video_url="https://www.youtube.com/watch?v=M5TcDyLs7dA"
+yt-dlp -o "【メン限】[%(upload_date)s]%(title)s.%(ext)s" $video_url -f bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a] -S vcodec:hevc_nvenc --cookies .\tmp\www.youtube.com_cookies.txt --download-archive acv
+
+# command to download youtube menbership video
+$video_url="https://www.youtube.com/watch?v=6MJlqK0u5lg"
+yt-dlp -o "【メン限】[%(upload_date)s]%(title)s.%(ext)s" $video_url -f bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a] -S vcodec:hevc_nvenc --cookies .\tmp\www.youtube.com_cookies.txt --download-archive acv
+
+# command to download youtube menbership video
+$video_url="https://www.youtube.com/watch?v=bq4EJpII7Mg"
+yt-dlp -o "【メン限】[%(upload_date)s]%(title)s.%(ext)s" $video_url -f bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a] -S vcodec:hevc_nvenc --cookies .\tmp\www.youtube.com_cookies.txt --download-archive acv
+
+# command to download youtube menbership video
+$video_url="https://www.youtube.com/watch?v=pa0bRTf5DCI"
+yt-dlp -o "【メン限】[%(upload_date)s]%(title)s.%(ext)s" $video_url -f bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a] -S vcodec:hevc_nvenc --cookies .\tmp\www.youtube.com_cookies.txt --download-archive acv
+
+# command to download youtube menbership video
+$video_url="https://www.youtube.com/watch?v=oyCTvp9HYo4"
+yt-dlp -o "【メン限】[%(upload_date)s]%(title)s.%(ext)s" $video_url -f bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a] -S vcodec:hevc_nvenc --cookies .\tmp\www.youtube.com_cookies.txt --download-archive acv
+
+# command to download youtube menbership video
+$video_url="https://www.youtube.com/watch?v=UiMUibq22Tg"
+yt-dlp -o "【メン限】[%(upload_date)s]%(title)s.%(ext)s" $video_url -f bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a] -S vcodec:hevc_nvenc --cookies .\tmp\www.youtube.com_cookies.txt --download-archive acv
+
+# command to download youtube menbership video
+$video_url="https://www.youtube.com/watch?v=zPtPXhWM9aI"
+yt-dlp -o "【メン限】[%(upload_date)s]%(title)s.%(ext)s" $video_url -f bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a] -S vcodec:hevc_nvenc --cookies .\tmp\www.youtube.com_cookies.txt --download-archive acv
+
+# command to download youtube menbership video
+$video_url="https://www.youtube.com/watch?v=5k7hz3Yq0sM"
+yt-dlp -o "【メン限】[%(upload_date)s]%(title)s.%(ext)s" $video_url -f bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a] -S vcodec:hevc_nvenc --cookies .\tmp\www.youtube.com_cookies.txt --download-archive acv
+
+# command to download youtube menbership video
+$video_url="https://www.youtube.com/watch?v=FZOBSoaCAKE"
+yt-dlp -o "【メン限】[%(upload_date)s]%(title)s.%(ext)s" $video_url -f bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a] -S vcodec:hevc_nvenc --cookies .\tmp\www.youtube.com_cookies.txt --download-archive acv
+
